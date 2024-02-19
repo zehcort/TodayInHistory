c56962aaab5a6c7f005d494549152d4399db4c6e
Luis Alejandro Trochez Arredondo <luis.trochez@globant.com>
2024-02-19 08:24:52 -0500

    Adding back JUnit libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,9 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
 
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
